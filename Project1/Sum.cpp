ab1c8c865ba481c34ea132345de9ad0e3ca33676
少鹏 邹 <727806600@qq.com>
2023-10-11 16:47:53 +0800

    求和

---
+++ Project1/Sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int sum_function(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	int sum = sum_function(a, b);
+	cout << sum << endl;
+	system("pause");
+	return 0;
+}
--- Project1/hello_world.cpp
+++ Project1/hello_world.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-int main()
+int main1()
 {
 	cout << "hello world" << endl;
 
